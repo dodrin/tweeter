100527afe39147da661ae6548dd0d027526088d9
Rina Inada <indrn@icloud.com>
2023-07-14 03:26:38 +0000

    Implement createTweets and renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,77 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+function calculateDaysAgo(timestamp) {
+  const millisecondsPerDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
+  const currentDate = new Date();
+  const tweetDate = new Date(timestamp);
+  const timeDiff = Math.abs(currentDate.getTime() - tweetDate.getTime());
+  const daysAgo = Math.floor(timeDiff / millisecondsPerDay);
+  return daysAgo;
+}
+
+$(document).ready(() => {
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+
+  const renderTweets = function (tweets) {
+    // alert('Hello Hi')
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    $("#tweets-container").empty();
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $("#tweets-container").prepend($tweet);
+    }
+  };
+
+  const createTweetElement = function (tweet) {
+    // fix the object with tweet not data
+    const article = $("<section>");
+    const daysAgo = calculateDaysAgo(tweet.created_at);
+
+    article.append(`<article class="tweet">
+    <header>
+    <div class="user-name">${tweet.user.name}</div>
+    <div class="user-id">${tweet.user.handle}</div>
+    </header>
+    <p>${tweet.content.text}</p>
+    <footer>
+    ${daysAgo} days ago
+    <div>
+      <i class="icon fa-solid fa-flag"></i>
+      <i class="icon fa-solid fa-retweet"></i>
+      <i class="icon fa-solid fa-heart"></i>
+    </div>
+    </footer>
+    </article>`);
+
+    return article;
+  };
+
+  renderTweets(data);
+});
